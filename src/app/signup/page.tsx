e738dec0b0393c6481a62dc6dae571733ea7d838
tushal-pethani <pethanitushal@gmail.com>
2025-09-15 02:13:19 +0530

    removed Oauth for temporary

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -215,7 +215,7 @@ export default function SignupPage() {
             <div style={{ flex: 1, height: 1, backgroundColor: "#e2e8f0" }}></div>
           </div>
 
-          <button
+          {/* <button
             onClick={() => signIn("google")}
             style={{
               padding: 14,
@@ -249,7 +249,7 @@ export default function SignupPage() {
               <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
             </svg>
             Sign up with Google
-          </button>
+          </button> */}
 
           <div style={{
             marginTop: 30,
